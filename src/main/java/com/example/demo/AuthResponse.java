5e1b520eeebc278e02d8b7633704f8e9082a3824
nikita <nikita.malkov.2002@gmail.com>
2025-05-25 17:22:10 +0400

    добавил зависимости

---
+++ src/main/java/com/example/demo/AuthResponse.java
@@ -0,0 +1,7 @@
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String message;
+    private Long userId;
+}
--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,4 +1,26 @@
-package com.example.demo;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "user"; // По умолчанию "user"
+
+    private boolean isBlocked = false;
+    private int eloRating = 1000;
 }
